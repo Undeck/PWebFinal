52ac2c489e74330be1099ea7441c75a077e954c1
Undeck <omaravendanogaribay@hotmail.com>
2023-11-25 23:38:06 -0600

    Se agrega index.php

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Programacion Web 1 Final</title>
+</head>
+<body>
+
+    <table border="1">
+        <caption>PWeb 1 Final</caption>
+        <tr>
+            <td><a href="sesion.php">Iniciar sesión</a></td>
+        </tr>
+        <tr>
+            <td><a href="registro.php">Registrarme</a></td>
+        </tr>
+    </table>
+
+</body>
+</html>
