6f0ca3185620f8679282512a74181ab6bae49cda
Undeck <omaravendanogaribay@hotmail.com>
2023-11-25 23:43:21 -0600

    Se agrega db.sql

---
+++ db.sql
@@ -0,0 +1,15 @@
+drop database if exists Pweb1;
+create database if not exists Pweb1;
+use Pweb1;
+create table usuarios(id int not null auto_increment primary key,
+						nombre varchar(50) not null,
+                        usuario varchar(50) not null,
+                        no_cuenta int(10) not null,
+                        email varchar(50), 
+                        pass varchar(20) not null,                        
+                        numero int(10), 
+                        credencial int(11) not null default '2');  
+insert into usuarios (nombre, usuario, no_cuenta, email, pass, num, credencial) values('Omar Avendano', 'OmarA', '319011003', 'undeck@gmail.com', 'Omar1234', '5540376746', '2');                                            
+insert into usuarios (nombre, usuario, no_cuenta, email, pass, num, credencial) values('Admin', 'Admin', '1234567890', 'Admin@gmail.com', 'Admin', '1234567890', '1');
+
+select * from usuarios;
